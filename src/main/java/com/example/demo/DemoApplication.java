11b7180a1c7839ae884fcfdf5a26453ef578c4aa
Pavan <you@example.com>
2018-05-09 22:28:32 -0500

    commit in master

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Update 3");
 		System.out.println("New Update1");
 	}
 }
