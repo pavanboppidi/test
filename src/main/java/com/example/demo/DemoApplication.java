93b93ec98080192d074308aec58375d579be9601
Pavan <you@example.com>
2018-05-09 22:25:31 -0500

    test2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 package com.example.demo;
 
@@ -10,7 +11,22 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("Update 3");
-		System.out.println("New Update1");
+		System.out.println("New Update 2");
 	}
 }
 
+=======
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("New Update2");
+	}
+}
+>>>>>>> test2
