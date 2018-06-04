22a5bddfdd84abc73903e9c86024847280b27d6c
Pavan <you@example.com>
2018-06-04 10:20:35 -0500

    test revert1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
@@ -10,17 +9,12 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("Update 3");
-		//gfjhj
-		//CHanges in 1
 		System.out.println("New Update 2 678924235435");
 		System.out.println("added in local");
-		//fghghjhgkhjkjh
 		System.out.println("added infgm");
 		System.out.println("added infgm...........");
-		//tyjyuyyu
 		System.out.println("added in localgnvbnbndgdfgm");
-		//test in br2
-		//changes in ws2
+		//test in revret2
 	}
 }
 
